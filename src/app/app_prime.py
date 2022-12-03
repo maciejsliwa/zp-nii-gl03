a24c2bf9a21ff633d3d8cdc865948abde00948f9
Maciej Śliwa <sliwa@ks-software.com>
2022-12-03 16:17:04 +0100

    Updated API. Authorization added.

--- src/app/app_prime.py
+++ src/app/app_prime.py
@@ -1,13 +1,21 @@
+import http.client
+import json
 import os
 import sympy
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from datetime import datetime
 
 app = FastAPI()
+with open('config.json', 'r') as f:
+    CONFIG = json.load(f)
+MY_DOMAIN = os.environ.get('AUTH0_SRV', CONFIG['SRV'])
+PAYLOAD = os.environ.get('AUTH0_PAYLOAD', CONFIG['PAYLOAD'])
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World", 'author': os.environ['MY_SECRET']}
+    return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
@@ -21,3 +29,31 @@ async def is_prime_number(number: str):
     if number.isnumeric():
         ret['return'] = sympy.isprime(int(number))
     return ret
+
+
+@app.get("/token")
+async def get_token():
+    conn = http.client.HTTPSConnection(MY_DOMAIN)
+    headers = {'content-type': "application/json"}
+    conn.request("POST", "/oauth/token", PAYLOAD, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
+    if not res.status == 200:
+        raise HTTPException(status_code=401, detail="Credentials invalid")
+    else:
+        return data['access_token']
+
+
+@app.get("/time")
+async def get_time(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    conn = http.client.HTTPSConnection(MY_DOMAIN)
+    headers = {
+        'content-type': "application/json",
+        'authorization': f"Bearer {token.credentials}"
+    }
+    conn.request("GET", "/api/v2/clients", headers=headers)
+    res = conn.getresponse()
+    if not res.status == 200:
+        raise HTTPException(status_code=401, detail="Token expired or invalid")
+    else:
+        return datetime.now().time()
